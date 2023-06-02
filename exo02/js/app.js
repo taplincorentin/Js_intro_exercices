583c71a57c846cc5341f57e986cc66e964e09fea
Corentin Taplin <taplincorentin@gmail.com>
2023-06-02 16:01:11 +0200

    made box go back to initial state onclick

--- exo02/js/app.js
+++ exo02/js/app.js
@@ -8,6 +8,9 @@ for(let i=1; i<=4; i++){
     container.appendChild(newbox)
     
     newbox.addEventListener("click",function(){
-        newbox.classList.add("box-clicked")
+        if(newbox.classList.contains("box-clicked")){
+            newbox.classList.remove("box-clicked")
+        }
+        else{newbox.classList.add("box-clicked")}
     })
 }
